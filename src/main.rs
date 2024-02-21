19fee11c71a3310b11fcf63802056580b6de86f7
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-02-21 15:18:50 +0700

    feat: change my mind add sophomore

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use tokio::sync::{Mutex, Semaphore};
 use std::sync::Arc;
-use reqwest::Error;
+use reqwest::{Client, Error};
 use scraper::{Html, Selector};
 
 use async_crawler::url_manager::UrlManager;
 
-async fn fetch_url(url: &str) -> Result<String,Error> {
-    let response = reqwest::get(url).await?;
+async fn fetch_url(client: &Client, url: &str) -> Result<String,Error> {
+    let response = client.get(url).send().await?;
     let body = response.text().await?;
     Ok(body)
 }
@@ -31,16 +31,21 @@ fn extract_links(html: &str) -> Vec<String> {
 #[tokio::main]
 async fn main() {
 let url_manager = Arc::new(Mutex::new(UrlManager::new()));
-    
-
+    let semaphore = Arc::new(Semaphore::new(10));
+    let client = Arc::new(Client::new());
+    {
+        let mut manager = url_manager.lock().await;
+        manager.add_url(vec![String::from("https://www.dek-d.com/tcas/62043/")]);
+    }
     let mut handles = vec![];
 
     for _ in 0..10 {
         let manager_clone = url_manager.clone();
-
+        let semaphore_clone = semaphore.clone();
+        let client_clone = client.clone();
         let handle = tokio::spawn(async move {
             loop {
-
+                let permit = semaphore_clone.acquire().await.expect("Failed to acquire semaphore permit");
                 let url = {
                     let mut manager = manager_clone.lock().await;
                     manager.get_next_url()
@@ -48,9 +53,10 @@ let url_manager = Arc::new(Mutex::new(UrlManager::new()));
 
                 match url {
                     Some(url) => {
-                        match fetch_url(&url).await {
+                        match fetch_url(&client_clone, &url).await {
                             Ok(body) => {
                                 let links = extract_links(&body);
+                                println!("Extracted {} links from {}", links.len(), url);
                                 let mut manager = manager_clone.lock().await;
                                 manager.add_url(links);
                             }
@@ -59,8 +65,12 @@ let url_manager = Arc::new(Mutex::new(UrlManager::new()));
                             }
                         }
                     },
-                    None => break,
+                    None => {
+                        drop(permit);
+                        break;
+                    }
                 }
+                drop(permit);
             }
 
         });
